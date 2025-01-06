4408b66dcf5de1314a81176f95bc6f2a5fb1ae6f
Kyaw-Thiha <k.thiha10.mail@gmail.com>
2025-01-06 01:23:43 -0500

    Partial Implementation of ClubDetailView

--- src/server/auth/authSendRequest.ts
+++ src/server/auth/authSendRequest.ts
@@ -16,6 +16,7 @@ export async function sendVerificationEmail(params: Params) {
       subject: "UofT Clubs Verification",
       react: MagicLinkEmail({ magicLink }),
     });
+    console.log(magicLink);
 
     if (error) {
       return Response.json({ error }, { status: 500 });
